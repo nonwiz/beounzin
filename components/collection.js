e73a0bf82a2fc67c032a7596263f952268a96ce6
Non Wiz <nonwiz@pm.me>
2022-08-03 21:58:56 +0700

    Update: update collection layout

--- components/collection.js
+++ components/collection.js
@@ -1,8 +1,10 @@
 
 import { Button, Card, Row, Col, Image, Radio, Text, Grid } from '@nextui-org/react';
 import CardGrid from "./cardGrid";
+import { useState, useEffect } from "react";
 
 export default function Garage() {
+  const [isMd, setIsMd] = useState(false);
 
   const collection = [
     {
@@ -63,14 +65,21 @@ export default function Garage() {
     }
   ]
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const matchMd = window.matchMedia("(min-width: 700px)")
+      setIsMd(matchMd.matches);
+    }
+  }, [])
+
 
   return (
     <div className='my-2 flex lg:-my-40 lg:-mx-40 '>
       <div className="md:w-[800px]">
-        <div className="p-4 md:p-2">
+        <div className="px-2 py-4 md:p-2">
           <Grid.Container gap={2} justify="center">
             {collection.map((item, index) =>
-              <Grid key={index} xs={item.xs}>
+              <Grid key={index} xs={isMd ? item.xs : 12}>
                 <CardGrid bg={item.bg} width={item.width} height={item.height} category={item.category} subcategory={item.subcategory} demo={item.demo} description={item.description} />
               </Grid>
             )}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Button, Container, Image, Radio, Text, useTheme } from '@nextui-org/react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
-import { Grid } from "@nextui-org/react";
+import { Grid, Spacer, Dropdown, Avatar } from "@nextui-org/react";
 import Lobby from "../components/lobby";
 import Garage from "../components/garage";
 import Contact from "../components/contact";
@@ -29,9 +29,7 @@ export default function Home() {
       const matchMd = window.matchMedia("(min-width: 700px)")
       setIsMd(matchMd.matches);
     }
-
-
-  })
+  }, [])
 
   return (
     <div>
@@ -49,22 +47,53 @@ export default function Home() {
 
             <Grid xs={2}>
               <div className={`flex flex-col -mx-4 md:mx-2 ${isMd ? 'px-10' : 'px-0'}`}>
-                <Radio.Group label="Navigation"
-                  orientation={isMd ? "vertical" : "horizontal"}
-                  defaultValue="lobby"
-                  onChange={selectNavi}
-                  value={navi}
-                  color="primary"
-                  labelColor="black"
-                >
-                  <Radio value="lobby" size={isMd ? "md" : "xs"}>LOBBY</Radio>
-                  <Radio value="garage" size={isMd ? "md" : "xs"}>GARAGE</Radio>
-                  <Radio value="collection" size={isMd ? "md" : "xs"}>COLLECTION</Radio>
-                  <Radio value="contact" size={isMd ? "md" : "xs"}>CONTACT</Radio>
-                </Radio.Group>
+                <Container xs>
+                  <Radio.Group label="Navigation"
+                    orientation={isMd ? "vertical" : "horizontal"}
+                    defaultValue="lobby"
+                    onChange={selectNavi}
+                    value={navi}
+                    color="primary"
+                    labelColor="black"
+                  >
+                    <Radio value="lobby" size={isMd ? "md" : "xs"}>LOBBY</Radio>
+                    <Radio value="garage" size={isMd ? "md" : "xs"}>GARAGE</Radio>
+                    <Radio value="collection" size={isMd ? "md" : "xs"}>COLLECTION</Radio>
+                    <Radio value="contact" size={isMd ? "md" : "xs"} className="hidden md:block">CONTACT</Radio>
+                  </Radio.Group>
+
+                </Container>
               </div>
             </Grid>
           </Grid.Container>
+          <div className="fixed bottom-0 right-0 m-2 md:hidden">
+            <Dropdown placement="bottom-left">
+              <Dropdown.Trigger>
+                <Button className="shadow z-10 bg-gray-400 p-1 w-10 h-10 rounded-full" auto>
+                  <>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                    </svg>
+                  </>
+                </Button>
+              </Dropdown.Trigger>
+              <Dropdown.Menu color="primary" aria-label="Avatar Actions">
+                <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                  <Text b color="inherit" css={{ d: "flex" }}>
+                    heabeoun@example.com
+                  </Text>
+                </Dropdown.Item>
+                <Dropdown.Item withDivider>
+                  Facebook
+                </Dropdown.Item>
+                <Dropdown.Item withDivider>
+                  Twitter
+                </Dropdown.Item>
+
+              </Dropdown.Menu>
+            </Dropdown>
+
+          </div>
 
         </main>
       </Container>
